8e50d08a650d5115d469decdfe392ecf30bdab85
tkdgus97 <breakking@naver.com>
2023-09-20 14:40:53 +0900

    프로그래머스 : 신규 아이디 추천

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -3,30 +3,13 @@ package programmers;
 import java.util.Stack;
 
 public class Solution {
-    public int solution(int[] ingredient) {
-        int answer = 0;
-        Stack<Integer> stack = new Stack<>();
-        for (int x : ingredient) {
-            stack.push(x);
-            int size = stack.size();
-            if (stack.size() >= 4) {
-                if (stack.get(size - 4) == 1
-                    && stack.get(size - 3) == 2
-                    && stack.get(size - 2) == 3
-                    && stack.get(size - 1) == 1) {
-                    answer++;
-                    for (int i = 0; i < 4; i++) {
-                        stack.pop();
-                    }
-                }
-            }
-        }
-        return answer;
+    public String solution(String new_id) {
+
     }
 
     public static void main(String[] args) {
         Solution M = new Solution();
-        System.out.println(M.solution(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1}));
+
     }
 
 }

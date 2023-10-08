c856d1a440d3ce839abefdadbc4b8cb2eff69392
tkdgus97 <breakking@naver.com>
2023-10-08 16:22:39 +0900

    fix

--- src/inflearn/sec4/AvgSaleSum.java
+++ src/inflearn/sec4/AvgSaleSum.java
@@ -1,5 +1,29 @@
 package inflearn.sec4;
 
-public class AvgSaleSum {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+public class AvgSaleSum {
+    public List<Integer> solution(int N, int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int lt = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            hm.put(arr[i], hm.getOrDefault(arr[i],0) + 1 );
+        }
+
+        for (int rt = N - 1; rt < arr.length; rt++) {
+            hm.put(arr[rt], hm.getOrDefault(arr[rt],0) + 1);
+            answer.add(hm.size());
+            hm.put(arr[lt], hm.get(arr[lt]) - 1);
+            if (hm.get(arr[lt]) == 0){
+                hm.remove(arr[lt]);
+            }
+            lt++;
+        }
+
+        return answer;
+    }
 }

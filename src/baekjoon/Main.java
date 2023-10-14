e9429935546baff265dcc8c4d390b6ed02e76a2a
tkdgus97 <breakking@naver.com>
2023-10-14 23:22:53 +0900

    백준 : 단어 나누기

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,5 +1,8 @@
 package baekjoon;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 //문제 풀이용
@@ -11,7 +14,26 @@ public class Main {
 
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        String s = sc.next();
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 1; i <= s.length() - 2; i++) {
+            for (int j = i + 1; j <= s.length() - 1; j++) {
+                StringBuilder s1 = new StringBuilder(s.substring(0, i));
+                StringBuilder s2 = new StringBuilder(s.substring(i, j));
+                StringBuilder s3 = new StringBuilder(s.substring(j, s.length()));
+
+                String tmp = s1.reverse().append(s2.reverse()).append(s3.reverse()).toString();
+
+                list.add(tmp);
+            }
+        }
+
+        Collections.sort(list);
+
+        System.out.println(list.get(0));
     }
 
 

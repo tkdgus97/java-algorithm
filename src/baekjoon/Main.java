ca6ad96fd43c32304f1b8a6a20ec2f85ab14fd63
tkdgus97 <breakking@naver.com>
2023-10-16 18:16:42 +0900

    백준 : 안전 영역

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,48 +1,57 @@
 package baekjoon;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 //문제 풀이용
 public class Main {
+    static int N;
 
-    //    public static String sol(int n, int k) {
-//        return null;
-//    }
-    static class Point {
-
-        int x;
-        int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-
+    static int[] dx = {-1, 1, 0, 0 };
+    static int[] dy = {0 , 0, -1, 1};
+    static int[][] map;
+    static boolean[][] visit;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        List<Point> list = new ArrayList<>();
+
+        N = sc.nextInt();
+        map = new int[N][N];
+
+        int max = 0;
         for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-
-            list.add(new Point(x, y));
+            for (int j = 0; j < N; j++) {
+                map[i][j] = sc.nextInt();
+                if (map[i][j] > max) {
+                    max = map[i][j];
+                }
+            }
         }
+        int res = 0;
 
-        list.sort((o1, o2) -> {
-            if (o1.x == o2.x) return o1.y - o2.y;
-            else return o1.x - o2.x;
-        });
-
-        for (Point p : list) {
-            System.out.println(p.x + " " + p.y);
+        for (int i = 0; i <= max; i++) {
+            int cnt = 0;
+            visit = new boolean[N][N];
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    if (map[j][k] > i && !visit[j][k]) {
+                        cnt++;
+                        DFS(j, k, i);
+                    }
+                }
+            }
+            res = Math.max(res, cnt);
         }
+        System.out.println(res);
     }
 
+    public static void DFS(int x, int y, int heigh) {
+        visit[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < N && ny < N && map[nx][ny] > heigh && !visit[nx][ny]) {
+                DFS(nx, ny, heigh);
+            }
+        }
+    }
 
 }

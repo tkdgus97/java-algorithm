5ee389aae221807e0edb485b6608ab36c7e023f9
tkdgus97 <breakking@naver.com>
2023-10-16 20:22:23 +0900

    pull commit

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,8 +1,5 @@
 package baekjoon;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 //문제 풀이용
@@ -11,38 +8,44 @@ public class Main {
     //    public static String sol(int n, int k) {
 //        return null;
 //    }
-    static class Point {
-
-        int x;
-        int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        List<Point> list = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] s = sc.nextLine().split(" ");
 
-            list.add(new Point(x, y));
+        int year = Integer.parseInt(s[2]);
+        int totalDay = 365;
+        int month = 0;
+        //윤년 체크
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            totalDay += 1;
         }
 
-        list.sort((o1, o2) -> {
-            if (o1.x == o2.x) return o1.y - o2.y;
-            else return o1.x - o2.x;
-        });
-
-        for (Point p : list) {
-            System.out.println(p.x + " " + p.y);
+        int totalMin = totalDay * 24 * 60 * 60 ;
+        //무슨 월인지 체크
+        for (int i = 0; i < months.length; i++) {
+            if (months[i].equals(s[0])) {
+                month = i + 1;
+                break;
+            }
         }
+        int tmpDay = 0;
+        String[] t = s[3].split(":");
+        int tmpMin =((Integer.parseInt(t[0])*60)+ Integer.parseInt(t[1])) * 60;
+        for (int i = 1; i < month; i++) {
+            if (i == 2) {
+                if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                    tmpDay += 1;
+                }
+            }
+            tmpDay += days[i - 1];
+        }
+        tmpDay += Integer.parseInt(s[1].replace(",", "")) - 1;
+        int tmpTotalMin = (tmpDay * 24 * 60 * 60) + tmpMin;
+
+
+        System.out.println((double)((double) tmpTotalMin / (double) totalMin) * 100);
     }
-
 
 }

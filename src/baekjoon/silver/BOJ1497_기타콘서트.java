231b555e5fa14123d740b689f4354844045928a7
tkdgus97 <breakking@naver.com>
2024-02-08 00:04:07 +0900

    BOJ : 1497 기타콘서트

--- src/baekjoon/silver/BOJ1497_기타콘서트.java
+++ src/baekjoon/silver/BOJ1497_기타콘서트.java
@@ -8,8 +8,7 @@ import java.util.StringTokenizer;
 
 public class BOJ1497_기타콘서트 {
 
-    private static int min = -1;
-
+    //곡의 개수를 연주하는데 필요한 기타 수 저장 배열 EX)3번째에 1이 들어가있다면 3곡을 연주하는데 1개의 기타만 필요하다는 의미
     private static int[] countArr;
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
@@ -21,6 +20,7 @@ public class BOJ1497_기타콘서트 {
 
         boolean[][] info = new boolean[N][M];
 
+        int min = -1;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
@@ -36,44 +36,66 @@ public class BOJ1497_기타콘서트 {
             }
         }
 
+        //하나도 연주 못하면 -1
+        if (min == -1) {
+            System.out.println(min);
+            return;
+        }
+
+        //최소값을 구해야 하므로 IMV로 초기화
         countArr = new int[M + 1];
         Arrays.fill(countArr, Integer.MAX_VALUE);
-        if (min == -1) {
-            System.out.println(-1);
-        } else {
-            min = Integer.MAX_VALUE;
-            dfs(0, M,N,0, new boolean[M], info);
-            System.out.println(countArr[M]);
+
+        //조합 별로 구함
+        for (int i = 1; i <= N; i++) {
+            dfs(0,0,new int[i], i, new boolean[N], info);
         }
 
-
+        for (int i = M; i > 0 ; i--) {
+            if (countArr[i] == Integer.MAX_VALUE) {
+                continue;
+            }
+            System.out.println(countArr[i]);
+            break;
+        }
     }
 
-    private static void dfs(int L, int M, int N, int count, boolean[] now, boolean[][] info) {
-        if (countArr[count] < L) {
-            return;
-        }
-        //종료 조건
-        if (L == N) {
-            countArr[count] = Math.min(L, countArr[count]);
+    /**
+     * @param L         - depth
+     * @param idx       - 조합 다음 idx
+     * @param now       - 현재 기타 조합 idx ex) now에 1,2가 있다면 입력받은 기타의 1,2번을 조합한 상태
+     * @param MAX       - 조합 개수를 의미 1이면 nC1을 의미
+     * @param check     - 현재 now에 들어있는지 없는지 체크 배열
+     * @param songInfo  - 기타가 어떤 곡들을 연주하는지 정보
+     */
+    private static void dfs(int L, int idx, int[] now, int MAX, boolean[] check, boolean[][] songInfo) {
+        if (L == MAX) {
+            int count = 0;
+            boolean[] ch = new boolean[songInfo[0].length];
+            //조합한 기타들로 몇곡을 연주할 수 있는지 확인
+            for (int i = 0; i < MAX; i++) {
+                for (int j = 0; j < songInfo[0].length; j++) {
+                    //ch가 true라면 이미 다른 기타가 연주 가능한것
+                    if (songInfo[now[i]][j] && !ch[j]) {
+                        ch[j] = true;
+                        count += 1;
+                    }
+                }
+            }
+
+            countArr[count] = Math.min(countArr[count], MAX);
             return;
         }
 
-        for (int i = 0; i < M; i++) {
-            if (info[L][i] && !now[i]) {
-                now[i] = true;
-                count += 1;
+        for (int i = idx; i < check.length; i++) {
+            if (check[i]) {
+                continue;
             }
+            now[L] = i;
+            check[i] = true;
+            dfs(L + 1, i + 1,now, MAX, check, songInfo);
+            check[i] = false;
         }
-        countArr[count] = L + 1;
-        dfs(L  + 1, M, N, count, now, info);
-        for (int i = 0; i < M; i++) {
-            if (!info[L][i] && now[i]) {
-                now[i] = false;
-                count -= 1;
-            }
-        }
-        countArr[count] = L;
-        dfs(L + 1, M, N, count, now, info);
     }
+
 }
